2480b9c3b6c862785c61031a4d0ab0bd9928e788
SICARIOXSICARIO <jwolfe27314@gmail.com>
2023-11-25 00:29:20 -0700

    fix

---
+++ src/main/java/com/thecybersphere/TheCyberSphereApplication.java
@@ -0,0 +1,13 @@
+package com.thecybersphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCyberSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCyberSphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thecybersphere/demo/TheCyberSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecybersphere.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCyberSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
